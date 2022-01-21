acd71c65bf6c2ab8d32050e8d4687d3d882564ed
Rushali <rpatnaik78@gmail.com>
2022-01-21 21:25:04 +0530

    21.01.2022

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define f first
+#define s second
+#define arr(a,n) long long int *a = new long long int[n]
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
+ll gcd(ll x, ll y){if(y>x){return gcd(y,x);}if(y==0){return x;}return gcd(y,x%y);}
+bool prime(ll x){for(ll i=2;i*i<=x;i++){if(x%i==0){return 0;}}return 1;}
+ll fact(ll n){if(n==0){return 1;}return n*fact(n-1);}
+bool powerOf2(ll n){if(n==0){return 0;} return (ceil(log2(n))== floor(log2(n)));}
+
+
+void solve(){
+ string s; cin>>s;
+ int c=0;
+ unordered_map<char,int>map;
+ for(int i=0;s[i]!='\0';i++){
+     map[s[i]]++;
+     if(map[s[i]]==1) c++;
+ } if(c%2==0) cout<<"CHAT WITH HER!";
+   else cout<<"IGNORE HIM!";
+}
+
+
+int32_t main(){
+fast
+ll t=1;
+//cin>>t;
+while(t--)
+solve();
+return 0;
+}
