a182a22fd419dbdf0748ae51098b43bebd34d22a
Rushali Patnaik <85608940+rushalipatnaik@users.noreply.github.com>
2022-01-26 23:46:57 +0530

    Add files via upload

--- A_Bit_.cpp
+++ A_Bit_.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 2147483648
+#define MIN -2147483647
 #define mod 1000000007
 #define f first
 #define s second
+#define min3(a, b, c) min(c, min(a, b))
+#define max3(a,b,c) max(c, max(a,b))
 #define arr(a,n) long long int *a = new long long int[n]
 #define ll long long int
+#define mp make_pair
+#define pb push_back
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
 
@@ -15,13 +21,16 @@ bool powerOf2(ll n){if(n==0){return 0;} return (ceil(log2(n))== floor(log2(n)));
 
 
 void solve(){
- ll n; cin>>n;
- ll x=0;
- while(n--){
-     string s; cin>>s;
-     if(s[1]=='+') x++;
-     else x--;
- } cout<<x;
+ll n;cin>>n;
+ll x=0;
+while(n--){
+    string s;cin>>s;
+    if(s=="++X" || s=="X++")
+    x+=1;
+    else
+    x-=1;
+}
+cout<<x<<endl;
 }
 
 
