8d9ab0085d1d1bcbb1178d1ddc50322daca6fd74
Rushali Patnaik <85608940+rushalipatnaik@users.noreply.github.com>
2022-01-29 10:38:19 +0530

    cp

--- A_Bear_and_Big_Brother.cpp
+++ A_Bear_and_Big_Brother.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 2147483648
+#define MIN -2147483647
 #define mod 1000000007
 #define f first
 #define s second
+#define min3(a, b, c) min(c, min(a, b))
+#define max3(a,b,c) max(c, max(a,b))
 #define arr(a,n) long long int *a = new long long int[n]
 #define ll long long int
+#define mp make_pair
+#define pb push_back
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
 
@@ -15,13 +21,14 @@ bool powerOf2(ll n){if(n==0){return 0;} return (ceil(log2(n))== floor(log2(n)));
 
 
 void solve(){
-ll x,y,c=0; cin>>x>>y;
-while(1){
-   c++;
-   x=3*x;
-   y=2*y;
-   if(x>y) break;
-} cout<<c;
+ll n,m;cin>>n>>m;
+ll y=0;
+while(n<=m){
+    n*=3;
+    m*=2;
+    y++;
+}
+cout<<y<<endl;
 }
 
 
